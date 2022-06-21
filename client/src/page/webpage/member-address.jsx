5d2db1f721d64b98e161966da7b3452ed3f85dc9
nattawat-wib <nattawat.wib@gmail.com>
2022-06-21 18:44:18 +0700

    edit address page

--- client/src/page/webpage/member-address.jsx
+++ client/src/page/webpage/member-address.jsx
@@ -1,4 +1,4 @@
-import { TextField, Grid, Button, Divider, Typography } from '@mui/material';
+import { TextField, Grid, Button, Divider, Typography, Dialog, DialogContent, DialogActions, IconButton } from '@mui/material';
 import { useState } from 'react';
 import LoadingButton from '@mui/lab/LoadingButton';
 import DialogConfirm from './../../components/util/dialog-confirm';
@@ -6,9 +6,16 @@ import DialogConfirm from './../../components/util/dialog-confirm';
 import AddIcon from '@mui/icons-material/Add';
 import DeleteIcon from '@mui/icons-material/Delete';
 import EditIcon from '@mui/icons-material/Edit';
+import CloseIcon from '@mui/icons-material/Close';
 
 export default function MemberAddress() {
     const [isConfirmDialogOpen, setIsConfirmDialogOpen] = useState(false);
+    const [isAddressDialogOpen, setIsAddressDialogOpen] = useState(false);
+
+    const handleEditAddress = () => {
+
+        setIsAddressDialogOpen(true)
+    }
 
     return (
         <>
@@ -17,9 +24,55 @@ export default function MemberAddress() {
                 setIsOpen={setIsConfirmDialogOpen}
                 callback={() => alert('success')}
             />
+            <Dialog
+                open={isAddressDialogOpen}
+                onClose={() => setIsAddressDialogOpen(false)}
+            >
+                <div className='flex justify-between items-center py-2 px-4'>
+                    Manage Address
+                    <IconButton onClick={() => setIsAddressDialogOpen(false)}>
+                        <CloseIcon />
+                    </IconButton>
+                </div>
+                <Divider />
+                <DialogContent>
+                    <TextField
+                        sx={{ mb: 3 }}
+                        size='small'
+                        label='firstname'
+                        fullWidth
+                    />
+                    <TextField
+                        sx={{ mb: 3 }}
+                        size='small'
+                        label='lastname'
+                        fullWidth
+                    />
+                    <TextField
+                        sx={{ mb: 3 }}
+                        size='small'
+                        label='tel'
+                        fullWidth
+                        type='number'
+                    />
+                    <TextField
+                        size='small'
+                        label='address'
+                        fullWidth
+                        multiline={true}
+                        rows={5}
+                    />
+                </DialogContent>
+                <Divider />
+                <DialogActions className='p-4'>
+                    <Button variant='contained' size='small'> Confirm </Button>
+                </DialogActions>
+            </Dialog>
+
             <div className='flex justify-between items-center'>
                 <Typography> <b> My Address </b> </Typography>
                 <Button
+                    onClick={() => setIsAddressDialogOpen(true)}
                     startIcon={<AddIcon />}
                     variant='contained'
                     size='small'
@@ -36,7 +89,14 @@ export default function MemberAddress() {
                                 <Grid xs={2} item className='text-right'> fullname : </Grid>
                                 <Grid xs={7} item> nutella tester </Grid>
                                 <Grid xs={3} item className='flex justify-end'>
-                                    <Button startIcon={<EditIcon />} size='small' sx={{ mr: 2 }}> Edit </Button>
+                                    <Button
+                                        onClick={handleEditAddress}
+                                        startIcon={<EditIcon />}
+                                        size='small'
+                                        sx={{ mr: 2 }}
+                                    >
+                                        Edit
+                                    </Button>
                                     <Button
                                         onClick={() => setIsConfirmDialogOpen(true)}
                                         startIcon={<DeleteIcon />}
@@ -50,7 +110,7 @@ export default function MemberAddress() {
                                 <Grid xs={2} item className='text-right'> Tel : </Grid>
                                 <Grid xs={7} item> 0987654321 </Grid>
                                 <Grid xs={3} item className='flex justify-end'>
-                                    <LoadingButton size='small'> set to default </LoadingButton>
+                                    <LoadingButton variant='outlined' size='small'> set to default </LoadingButton>
                                 </Grid>
                             </Grid>
                             <Grid spacing={2} container sx={{ mb: 1 }}>
