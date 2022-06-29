660f7d6ff5a5b9a33ee4d98401a4f4b4dbadbc35
nattawat-wib <nattawat.wib@gmail.com>
2022-06-29 16:33:25 +0700

    host heroku

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,7 +3,14 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [react()],
+    plugins: [react({
+        babel: {
+          plugins: [
+            ['@babel/plugin-transform-react-jsx', { runtime: 'automatic' }],
+          ]
+        },
+      })],
+	// plugins: [react()],
 	server: {
 		port: '3000'
 	}
