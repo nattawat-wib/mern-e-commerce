90ea09a22ca860252f49172599e53d5ac607a88f
nattawat-wib <nattawat.wib@gmail.com>
2022-06-29 15:40:19 +0700

    host heroku

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -33,7 +33,7 @@ import OrderManagement from './page/control-panel/order-management';
 
 import NotFound from './page/not-found';
 
-console.log("import.meta.env.VITE_BASE_API", import.meta.env.VITE_BASE_API);
+// console.log("import.meta.env.VITE_BASE_API", import.meta.env.VITE_BASE_API);
 
 const App = () => {
     return (
